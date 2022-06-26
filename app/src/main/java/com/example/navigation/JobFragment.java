0028d18b498a562b8823687b09f3b11e2d6f5935
Vatsa777 <93812665+Vatsa777@users.noreply.github.com>
2022-06-26 10:02:19 +0530

    Home Screen App

--- app/src/main/java/com/example/navigation/JobFragment.java
+++ app/src/main/java/com/example/navigation/JobFragment.java
@@ -1,5 +1,6 @@
 package com.example.navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class JobFragment extends Fragment {
-
+    Button postad;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_job, container, false);
+        View view = inflater.inflate(R.layout.fragment_job, container , false);
+
+        postad = view.findViewById(R.id.postad);
+
+        postad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),FindJob.class));
+            }
+        });
+        return view;
+
+
     }
 }
--- app/src/main/java/com/example/navigation/SettingFragment.java
+++ app/src/main/java/com/example/navigation/SettingFragment.java
@@ -1,5 +1,6 @@
 package com.example.navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class SettingFragment extends Fragment {
 
 
-
+    Button logout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_setting, container , false);
 
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        logout = view.findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),JobAd.class));
+            }
+        });
+        return view;
     }
 }
